bcb769b69641906b8758e8f0b5c2c53c5f2ed9ef
Chetan Ladkat <100022084+Chetan9777@users.noreply.github.com>
2024-09-02 13:13:00 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,52 @@
+
+async function getSongs(){
+    let a = await fetch("http://127.0.0.1:5500/songs/")
+    let response = await a.text();
+    let div = document.createElement("div")
+    div.innerHTML= response;
+    let as = div.getElementsByTagName("a")
+    let songs=[]
+    for(let index=0; index<as.length; index++)
+    {
+        const element = as[index];
+        if(element.href.endsWith(".mp3"))
+            {
+                songs.push(element.href.split("/songs/")[1])
+            }
+    }
+    return songs;
+}
+
+async function main()
+{
+    // get the list of all the songs
+    let songs =  await getSongs()
+    console.log(songs)
+
+    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0];
+
+    for (const song of songs) {
+        songUL.innerHTML= songUL.innerHTML+ `<li> 
+        <img src="./images/music.svg" alt="" class="invert">
+        <div class="info">
+            <div>${song}</div>
+            <div>song artist</div>
+        </div>
+        <div class="play_now">
+        <img src="./images/play_now.svg" alt="" class="invert">
+        </div>
+        </li>`;
+    }
+    // play the first song
+    var audio = new Audio(songs[0])
+    audio.play();
+
+    audio.addEventListener("loadeddata", ()=>{
+        let duration = audio.duration;
+        console.log(duration)
+    })
+
+
+}
+
+main()
